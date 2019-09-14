9a65fb8ff26ed0128efd576e03f90c32dd718e3d
pavan <namapavankumar7@gmail.com>
2019-09-14 23:00:36 +0530

    Chekio Home

--- Checkio/Home/XsandOsReferee.py
+++ Checkio/Home/XsandOsReferee.py
@@ -2,12 +2,35 @@ from typing import List
 
 
 def checkio(game_result: List[str]) -> str:
+    # checkio = lambda g: max((__import__('re')).match(r'(?:(?:...)*(X|O)\1\1)|(?:.*(X|O)(?:..\2..\2))|(?:(X|O)(?:...\3...\3))|(?:..(X|O)(?:.\4.\4))|()', ''.join(g)).groups() + ('D',), key=bool)
+    # s1, s2, s3 = g
+    # s4 = tuple(g[0][0] + g[1][1] + g[2][2])
+    # s5 = tuple(g[0][2] + g[1][1] + g[2][0])
+    # a = list(zip(s1, s2, s3))
+    # a.extend([s4, s5, tuple(g[0]), tuple(g[1]), tuple(g[2])])
+    # for x in range(len(a)):
+    #     if len(set(a[x])) == 1:
+    #         if a[x][0] in 'XO':
+    #             return a[x][0]
+    #     else: continue
+    # return 'D'
+
+    
+    if game_result[0][0] == game_result[1][1] == game_result[2][2] and game_result[0][0] != '.':
+        return game_result[0][0]
+    elif game_result[2][0] == game_result[1][1] == game_result[0][2] and game_result[0][2] != '.':
+        return game_result[2][0]
+
+    for row in game_result:
+        if row[0] == row[1] == row[2] and row[0] != '.':
+            return row[0]
+
     rotate_field = list(zip(*game_result))
     for field in rotate_field:
-        if all(field):
-            print('************', field[0])
-        # print('------------', field)
-    return "D" or "X" or "O"
+        if field[0] == field[1] == field[2] and field[0] != '.':
+            return field[0]
+
+    return "D"
 
 
 if __name__ == '__main__':
